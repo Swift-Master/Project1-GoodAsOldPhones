3b3c745d3bd56ae6cb7bc60a0b259ee8d23a04d2
openbible <59015538+OpenBible3438@users.noreply.github.com>
2023-04-20 00:34:26 +0900

    [20230419] UsViewController 스크롤뷰 구현

--- GoodAsOldPhones_DEV_1/GoodAsOldPhones_DEV_1/UsViewController.swift
+++ GoodAsOldPhones_DEV_1/GoodAsOldPhones_DEV_1/UsViewController.swift
@@ -7,24 +7,40 @@
 
 import UIKit
 
+// Us
 class UsViewController: UIViewController {
 
+    var usTopImage: UIImage!
+    @IBOutlet weak var usTopImageView: UIImageView!
+    
+    var emailImage: UIImage!
+    @IBOutlet weak var emailImageView: UIImageView!
+    
+    var telImage: UIImage!
+    @IBOutlet weak var telImageView: UIImageView!
+    
+    var urlImage: UIImage!
+    @IBOutlet weak var urlImageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("Us 화면")
+        // 상단 이미지 설정
+        usTopImage = UIImage(named: "header-contact")
+        usTopImageView.image = usTopImage
+        
+        // 하단 Contact 이미지 설정
+        emailImage = UIImage(named: "icon-about-email")
+        emailImageView.image = emailImage
+        
+        telImage = UIImage(named: "icon-about-phone")
+        telImageView.image = telImage
+        
+        urlImage = UIImage(named: "icon-about-website")
+        urlImageView.image = urlImage
+        
 
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- GoodAsOldPhones_DEV_1/GoodAsOldPhones_DEV_1/ViewController.swift
+++ GoodAsOldPhones_DEV_1/GoodAsOldPhones_DEV_1/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+// Products
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
