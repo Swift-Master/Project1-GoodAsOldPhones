519d896c7f88ae1b3c54738ee9a636e6313165ee
openbible <59015538+OpenBible3438@users.noreply.github.com>
2023-04-19 22:31:49 +0900

    Create Project

---
+++ GoodAsOldPhones_DEV_1/GoodAsOldPhones_DEV_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodAsOldPhones_DEV_1
+//
+//  Created by 편성경 on 2023/04/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
