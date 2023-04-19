fc714fc0425ccdcdbc042eb4094cec4212972645
openbible <59015538+OpenBible3438@users.noreply.github.com>
2023-04-19 22:52:00 +0900

    [20230419] TabBar 구현
    - UsViewController 생성, TabBarController 추가

---
+++ GoodAsOldPhones_DEV_1/GoodAsOldPhones_DEV_1/UsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UsViewController.swift
+//  GoodAsOldPhones_DEV_1
+//
+//  Created by 편성경 on 2023/04/19.
+//
+
+import UIKit
+
+class UsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Us 화면")
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
