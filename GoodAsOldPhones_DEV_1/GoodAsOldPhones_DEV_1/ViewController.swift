9765bfb4e7489c50ab6baa7fea505496f9787165
openbible <59015538+OpenBible3438@users.noreply.github.com>
2023-04-21 00:32:54 +0900

    [20230421] Products TableView 화면 이동 구현

--- GoodAsOldPhones_DEV_1/GoodAsOldPhones_DEV_1/ViewController.swift
+++ GoodAsOldPhones_DEV_1/GoodAsOldPhones_DEV_1/ViewController.swift
@@ -17,18 +17,10 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         productsTableView.dataSource = self
-        productsTableView.rowHeight = 100
+        productsTableView.delegate = self
     }
-
-    // 상세 화면 이동 버튼 TEST
-    /*
-    @IBAction func goDetail(_ sender: UIButton) {
-        let detailVC = self.storyboard?.instantiateViewController(identifier: "detail") as! DetailViewController
-        self.navigationController?.pushViewController(detailVC, animated: true)
-    }
-     */
-    
 }
 
 // Cell Class 따로 생성하지 않음 !
@@ -49,16 +41,30 @@ class ProductsTableViewCell: UITableViewCell {
 
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
     
+    // cell의 갯수
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return productsItem.count
     }
     
+    // 표시할 cell
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let productsCell = productsTableView.dequeueReusableCell(withIdentifier: "ProductsTableViewCell", for: indexPath) as! ProductsTableViewCell
         productsCell.cellImageView.image = UIImage(named: cellImage[indexPath.row])
         productsCell.cellLabel.text = productsItem[indexPath.row]
         
         return productsCell
+    }
+    
+    // cell 높이
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    // cell 선택 이벤트
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let detailVC = self.storyboard?.instantiateViewController(identifier: "detail") as! DetailViewController
+        detailVC.selectRow = indexPath.row // 선택한 index 전달
+        self.navigationController?.pushViewController(detailVC, animated: true)
     }
     
 }
